cbdb6e4b02f9a8962b252fcc5bc289f40c9ab17b
Sausainis777 <a.tankeliun@gmail.com>
2023-10-13 08:48:58 +0300

    Add methods

---
+++ OpenCartCoupons/src/main/java/org/example/CartPage.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    WebDriver driver;
+
+    By useCoupon = By.cssSelector("#accordion .accordion-item:nth-of-type(2) [data-bs-toggle]");
+
+    By inputCoupons = By.cssSelector("#input-coupon");
+
+    By clickToUseCoupon = By.cssSelector("form#form-coupon  .btn.btn-primary");
+
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+
+
+    }
+    public void useCoupon(){
+
+        var element = driver.findElement(useCoupon);
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
+    }
+    public void inputMyCoupons(String name){
+        driver.findElement(inputCoupons).sendKeys(name);
+
+    }
+    public void clickToUseCoupon(){
+        var element = driver.findElement(clickToUseCoupon);
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
+    }
+}
